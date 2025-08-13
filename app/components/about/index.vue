3c2a19695b99f970c520056466fdc7fdeb88ff37
Dinobergue Viana <dinbergueviana@MacBook-Air-de-Dinbergue.local>
2025-08-12 22:13:57 -0300

    feat: adiciona estrutura base da secao sobre mim

---
+++ app/components/about/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="h-screen grid grid-cols-2 py-11 px-12" id="about">
+    <div>
+      <h2 class="text-5xl font-bold">sobre mim</h2>
+    </div>
+  
+    <div>
+      <p>Entrei no mercado de programação como Desenvolvedor Web em 2021 após dicidir mudar de carreira no ano anterior.</p>
+      
+      <p>Atualmente estou cursando Análise e Desenvolvimento de Sistemas pela Uni Dom Bosco e possuo certificado em Desenvolvimento Web Full Stack Node pela Digital House.</p>
+      
+      <p>Além da faculdade, também sou aluno do curso.dev do Filipe Deschamps.</p>
+
+      <div>
+        <small>Em breve contarei com mais detalhes como foi a minha transição de carreira.</small>
+      </div>
+    </div>
+  </section>
+</template>
+
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <Header />
   <Hero />
+  <About />
 </template>
 
 <script setup>
 import Hero from "~/components/hero/hero.vue";
+import About from "@/components/about"
 </script>
