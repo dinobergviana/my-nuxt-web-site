e56282d1137e18f2c8e0fe1a3142f07a0f80ce05
Diobergue <vianadiobergue@gmail.com>
2025-08-12 22:49:17 -0300

    feat: estiliza seção sobre mim

--- app/components/about/index.vue
+++ app/components/about/index.vue
@@ -1,18 +1,18 @@
 <template>
   <section class="h-screen grid grid-cols-2 py-11 px-12" id="about">
-    <div>
+    <div class="text-center">
       <h2 class="text-5xl font-bold">sobre mim</h2>
     </div>
   
-    <div>
-      <p>Entrei no mercado de programação como Desenvolvedor Web em 2021 após dicidir mudar de carreira no ano anterior.</p>
+    <div class="text-justify text-lg">
+      <p class="mb-4">Entrei no mercado de programação como <strong>Desenvolvedor Web</strong> em 2021 após dicidir mudar de carreira no ano anterior.</p>
       
-      <p>Atualmente estou cursando Análise e Desenvolvimento de Sistemas pela Uni Dom Bosco e possuo certificado em Desenvolvimento Web Full Stack Node pela Digital House.</p>
+      <p class="mb-4">Atualmente <strong>estou cursando Análise e Desenvolvimento de Sistemas</strong> pela Uni Dom Bosco e <strong>possuo certificado em Desenvolvimento Web Full Stack Node</strong> pela <strong class="text-sky-600"><a href="https://www.digitalhouse.com/br" target="_blank">Digital House</a></strong>.</p>
       
-      <p>Além da faculdade, também sou aluno do curso.dev do Filipe Deschamps.</p>
+      <p class="mb-4">Além da faculdade, <strong>também sou aluno do</strong> <strong class="text-sky-600"><a href="https://curso.dev/">curso.dev </a></strong> do <strong class="text-sky-600"><a href="https://filipedeschamps.com.br/">Filipe Deschamps</a></strong>.</p>
 
       <div>
-        <small>Em breve contarei com mais detalhes como foi a minha transição de carreira.</small>
+        <small class="text-gray-500">Em breve contarei com mais detalhes como foi a minha transição de carreira.</small>
       </div>
     </div>
   </section>
