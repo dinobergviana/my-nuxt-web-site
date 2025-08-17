a2be004c63d981d328ae7edd0dd76b9594d685b4
Diobergue <vianadiobergue@gmail.com>
2025-08-17 09:27:09 -0300

    feat: crea a seção Experiêmcias

---
+++ app/components/experiences/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <section>
+    <hr class="my-8 border-t-2 border-gray-300 w-3/4 w-screen" />
+
+    <div class="grid grid-cols-2 px-24 pb-12 pt-4">
+      <div class="flex items-center justify-center text-gray-700">
+        <h3 class="text-5xl font-bold">Experiências</h3>
+      </div>
+
+      <div class="flex justify-center text-justify flex-col text-gray-600">
+        <div class="text-gray-600 mb-4">
+          <h3 class="text-lg text-gray-700 font-bold">Enjoei</h3>
+          <p class="text-base font-medium text-gray-600">
+            Desenvolvedor Frontend
+          </p>
+          <p>Mai 2024 - Mai 2025</p>
+          <span>Trabalhei com: </span>
+          <i>Vue.js, </i>
+          <i>Vuex, </i>
+          <i>Typescript, </i>
+          <i>RailsVitest, </i>
+          <i>Vue Test Utils.</i>
+        </div>
+
+        <div class="mb-4">
+          <h3 class="text-lg text-gray-700 font-bold">Educacros</h3>
+          <p>Desenvolvedor Frontend</p>
+          <p>Set 2022 - Abr 2024</p>
+          <span>Trabalhei com: </span>
+          <i>Vue.js, </i>
+          <i>Vuex, </i>
+          <i>bootstrap Vue, </i>
+          <i>Vuexy. </i>
+        </div>
+
+        <div class="mb-4">
+          <h3 class="text-lg text-gray-700 font-bold">Warren Investimentos</h3>
+          <p>Desenvolvedor Frontend</p>
+          <p>Jan 2022 - Ago 2022</p>
+          <span>Trabalhei com: </span>
+          <i>Vue.js, </i>
+          <i>Vuex, </i>
+          <i>Typescript.</i>
+        </div>
+
+        <div>
+          <h3 class="text-lg text-gray-700 font-bold">BoxTi</h3>
+          <p>Desenvolvedor Frontend</p>
+          <p>Mar 2021 - Dez 2021</p>
+          <span>Trabalhei com: </span>
+          <i>HTML, </i>
+          <i>CSS, </i>
+          <i>JavaScript, </i>
+          <i>Vue.js, </i>
+          <i>PHP, </i>
+          <i>Node.js, </i>
+          <i>MySQL.</i>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,9 +2,11 @@
   <Header />
   <Hero />
   <About />
+  <Experiences />
 </template>
 
 <script setup>
 import Hero from "~/components/hero/hero.vue";
 import About from "@/components/about";
+import Experiences from "@/components/experiences";
 </script>
