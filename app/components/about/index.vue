76ff7522469b8a06dca2c664cdf3dd7c06336f1b
Dinobergue Viana <dinbergueviana@MacBook-Air-de-Dinbergue.local>
2025-09-01 22:36:22 -0300

    feat: add about me traduction

--- app/components/about/index.vue
+++ app/components/about/index.vue
@@ -4,7 +4,7 @@
     id="about"
   >
     <div class="flex items-center justify-center m-0 p-0">
-      <h2 class="dark:text-white text-5xl md:text-5xl font-bold m-0 p-0">sobre mim</h2>
+      <h2 class="dark:text-white text-5xl md:text-5xl font-bold m-0 p-0">{{ $t("about.title") }}</h2>
     </div>
 
     <div
@@ -12,44 +12,37 @@
     >
       <div>
         <p class="dark:text-white mb-4">
-          Entrei no mercado de programação como
-          <strong>Desenvolvedor Web</strong> em 2021 após dicidir mudar de
-          carreira no ano anterior.
+          {{ $t("about.p1.partOne") }}
+          <strong>{{ $t("about.p1.strongText") }}</strong> {{ $t("about.p1.partTwo") }}
         </p>
 
         <p class="dark:text-white mb-4">
-          Atualmente
-          <strong>estou cursando Análise e Desenvolvimento de Sistemas</strong>
-          pela Uni Dom Bosco e
-          <strong
-            >possuo certificado em Desenvolvimento Web Full Stack Node</strong
-          >
-          pela
+          {{ $t("about.p2.partOne") }}
+          <strong>{{ $t("about.p2.strongTextOne") }}</strong> {{ $t("about.p2.partTwo") }}
+          <strong>{{ $t("about.p2.strongTextTwo") }}</strong> {{ $t("about.p2.partThree") }}
           <strong class="text-blue-600 hover:text-sky-500 ease-in"
             ><a href="https://www.digitalhouse.com/br" target="_blank"
-              >Digital House</a
+              >{{ $t("about.p2.strongTextLink.text") }}</a
             ></strong
           >.
         </p>
 
         <p class="dark:text-white mb-4">
-          Além da faculdade, <strong>também sou aluno do</strong>
+          {{ $t("about.p3.partOne") }} <strong>{{ $t("about.p3.strongTextOne") }}</strong>
           <strong class="text-blue-600 hover:text-sky-500"
-            ><a href="https://curso.dev/"> curso.dev </a></strong
+            ><a href="https://curso.dev/"> {{ $t("about.p3.strongTextLinkOne.text") }} </a></strong
           >
-          do
+          {{ $t("about.p3.partTwo") }}
           <strong class="text-blue-600 hover:text-sky-500"
             ><a href="https://filipedeschamps.com.br/"
-              >Filipe Deschamps</a
+              >{{ $t("about.p3.strongTextLinkTwo.text") }}</a
             ></strong
-          >.
+          >
         </p>
 
         <div>
           <small class="dark:text-gray-400 text-gray-500"
-            >Em breve contarei com mais detalhes como foi a minha transição de
-            carreira.</small
-          >
+            >{{ $t("about.p4.partOne") }}</small>
         </div>
       </div>
     </div>
--- app/components/footer/index.vue
+++ app/components/footer/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="">
+  <footer class="bg-white dark:bg-gray-800">
     <p class="p-4 text-center text-gray-500 font-medium">
       © 2025 Dinobergue Viana
     </p>
