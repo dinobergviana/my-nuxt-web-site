280978fd6557bfa2f5d15b9ab246b53367bc48b7
Dinobergue Viana <dinbergueviana@MacBook-Air-de-Dinbergue.local>
2025-09-01 21:53:01 -0300

    refactor: remove seções que não tem a ver com sobre mim

--- app/components/about/index.vue
+++ app/components/about/index.vue
@@ -1,23 +1,23 @@
 <template>
   <section
-    class="font-inter font-medium min-h-screen grid grid-cols-1 md:grid-cols-2 py-8 px-4 md:py-11 md:px-24 text-gray-700"
+    class="border-t border-gray-300 dark:border-t dark:border-gray-700 bg-white dark:bg-gray-800 font-inter font-medium min-h-screen grid grid-cols-1 md:grid-cols-2 py-8 px-4 md:py-11 md:px-24 text-gray-700"
     id="about"
   >
     <div class="flex items-center justify-center m-0 p-0">
-      <h2 class="text-5xl md:text-5xl font-bold m-0 p-0">Sobre mim</h2>
+      <h2 class="dark:text-white text-5xl md:text-5xl font-bold m-0 p-0">sobre mim</h2>
     </div>
 
     <div
-      class="flex items-center justify-center text-justify text-base md-text-lg md:m-0 p-0"
+      class="flex items-center justify-center text-justify text-base md-text-lg md:m-0 p-4"
     >
       <div>
-        <p class="mb-4">
+        <p class="dark:text-white mb-4">
           Entrei no mercado de programação como
           <strong>Desenvolvedor Web</strong> em 2021 após dicidir mudar de
           carreira no ano anterior.
         </p>
 
-        <p class="mb-4">
+        <p class="dark:text-white mb-4">
           Atualmente
           <strong>estou cursando Análise e Desenvolvimento de Sistemas</strong>
           pela Uni Dom Bosco e
@@ -25,20 +25,20 @@
             >possuo certificado em Desenvolvimento Web Full Stack Node</strong
           >
           pela
-          <strong class="text-blue-600 hover:text-sky-400 ease-in"
+          <strong class="text-blue-600 hover:text-sky-500 ease-in"
             ><a href="https://www.digitalhouse.com/br" target="_blank"
               >Digital House</a
             ></strong
           >.
         </p>
 
-        <p class="mb-4">
+        <p class="dark:text-white mb-4">
           Além da faculdade, <strong>também sou aluno do</strong>
-          <strong class="text-blue-600 hover:text-sky-400"
+          <strong class="text-blue-600 hover:text-sky-500"
             ><a href="https://curso.dev/"> curso.dev </a></strong
           >
           do
-          <strong class="text-blue-600 hover:text-sky-400"
+          <strong class="text-blue-600 hover:text-sky-500"
             ><a href="https://filipedeschamps.com.br/"
               >Filipe Deschamps</a
             ></strong
@@ -46,7 +46,7 @@
         </p>
 
         <div>
-          <small class="text-gray-500"
+          <small class="dark:text-gray-400 text-gray-500"
             >Em breve contarei com mais detalhes como foi a minha transição de
             carreira.</small
           >
