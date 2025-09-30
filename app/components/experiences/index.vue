d1597efaf28bc55e078c5356a16100cd4de4ecb5
Diobergue Viana <dioberguev@gmail.com>
2025-09-29 23:40:17 -0300

    feature: cria seção Experiências

---
+++ app/components/experiences/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="grid border-t border-gray-300 dark:border-t dark:border-gray-700  md:grid-cols-2 py-8 px-4 md:py-11 md:px-24 text-gray-700 dark:bg-gray-800">
+    <div class="flex items-center justify-center mx-auto py-[80px]">
+      <h2 class="dark:text-white text-5xl md:text-5xl font-bold m-0 px-4 ">Experiências</h2>
+    </div>
+
+    <div class="dark:text-white px-4">
+      <div class="mb-3 px-1">
+        <h3 class="font-bold text-lg">Enjoei</h3>
+        <p class=" text-gray-700 font-semibold dark:text-white">Desenvolvedor Frontend</p>
+        <p>Mai 2024 - Mai 2025</p>
+        <span>trabalhei com:</span>
+        <i>Vue.js</i>
+        <i>, Vuex</i>
+        <i>, Typescript</i>
+        <i>, RailsVitest</i>
+        <i>, Vue Test Utils</i>
+      </div>
+
+      <div class="mb-3 ">
+        <h3 class="font-bold ">Educacros</h3>
+        <p class=" text-gray-700 font-semibold">Desenvolvedor Frontend</p>
+        <p>Set 2022 - Abr 2024</p>
+        <span>trabalhei com:</span>
+        <i>Vue.js</i>
+        <i>, Vuex</i>
+        <i>, Bootstrap Vue</i>
+        <i>, Vuexy</i>
+      </div>
+
+      <div class="mb-3 ">
+        <h3 class="font-bold">Warren Investimentos</h3>
+        <p class=" text-gray-700 font-semibold">Desenvolvedor Frontend</p>
+        <p>Jan 2022 - Ago 2022</p>
+        <span>trabalhei com:</span>
+        <i>Vue.js</i>
+        <i>, Vuex</i>
+        <i>, Vuex.js</i>
+        <i>, Typescript</i>
+      </div>
+
+      <div class="mb-2 ">
+        <h3 class="font-bold">BoxTi</h3>
+        <p class=" text-gray-700 font-semibold">Desenvolvedor</p>
+        <p>Mar 2021- Dez 2021</p>
+        <span>trabalhei com:</span>
+        <i>HMTL</i>
+        <i>, CSS</i>
+        <i>, Javascript</i>
+        <i>, Vue.js</i>
+        <i>, PHP</i>
+        <i>, Node.js</i>
+        <i>, MySQL</i>
+      </div>
+    </div>
+  </section>
+</template>
