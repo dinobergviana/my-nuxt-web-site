25bcf4626f9d132558351568d49df426f179e93a
Diobergue <dioberguev@gmail.com>
2025-12-11 23:43:31 -0300

    feature/cria button voltar para topo da pagina

---
+++ app/components/buttons/VoltarTopo.vue
@@ -0,0 +1,11 @@
+<template>
+  <a
+    href="#top"
+    class="inline-block px-2 py-2 rounded-lg font-semibold text-gray-500 hover:text-gray-800 dark:hover:text-gray-400"
+  >
+    <span class="text-sm flex items-center gap-2">
+      <IconPhArrowUp size="16" weight="bold" aria-hidden="true" />
+      Voltar para topo
+    </span>
+  </a>
+</template>
--- app/components/footer/index.vue
+++ app/components/footer/index.vue
@@ -1,7 +1,19 @@
 <template>
-  <footer class="bg-white dark:bg-gray-800">
-    <p class="p-4 text-center text-gray-500 font-medium">
+  <footer
+    class="bg-white dark:bg-gray-800 flex items-center px-4 py-2 relative"
+  >
+    <p
+      class="absolute left-1/2 transform -translate-x-1/2 text-center text-gray-500 font-medium"
+    >
       © 2025 Dinobergue Viana
     </p>
+
+    <div class="ml-auto">
+      <VoltarTopo />
+    </div>
   </footer>
 </template>
+
+<script setup lang="ts">
+import VoltarTopo from "../buttons/voltarTopo.vue";
+</script>
