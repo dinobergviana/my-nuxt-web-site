922d39c966e10ab7e776b5762b6a3da7149f2c0f
Diobergue <dioberguev@gmail.com>
2025-12-12 22:10:49 -0300

    feature/botão voltar para topo no footer

--- app/components/footer/index.vue
+++ app/components/footer/index.vue
@@ -9,11 +9,15 @@
     </p>
 
     <div class="ml-auto">
-      <VoltarTopo />
+      <a
+        href="#hero"
+        class="inline-block px-2 py-2 rounded-lg font-semibold text-gray-500 hover:text-gray-800 dark:hover:text-gray-400"
+      >
+        <span class="text-sm flex items-center gap-2">
+          <IconPhArrowUp size="16" weight="bold" aria-hidden="true" />
+          Voltar para topo
+        </span>
+      </a>
     </div>
   </footer>
 </template>
-
-<script setup lang="ts">
-import VoltarTopo from "../buttons/voltarTopo.vue";
-</script>
