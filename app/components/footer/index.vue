460e0286328b5088bb7ac1021d2ab07485d0671e
Dinobergue Viana <dinbergueviana@MacBook-Air-de-Dinbergue.local>
2025-12-26 15:40:28 -0300

    style: change back to top button text color

--- app/components/footer/index.vue
+++ app/components/footer/index.vue
@@ -3,7 +3,7 @@
     class="px-6 py-2 bg-white dark:bg-gray-800 flex items-center justify-between md:justify-end"
   >
     <div class="w-[100%] md:w-[50%] flex items-center justify-between">
-      <p class="text-center text-gray-500 font-normal text-md">
+      <p class="text-center text-gray-400 font-normal text-md">
         © 2025 Dinobergue Viana
       </p>
 
@@ -11,7 +11,7 @@
         v-if="hasBackToTopButton"
         :href="backToTopPath"
         aria-label="Voltar para o topo"
-        class="inline-block py-2 rounded-lg font-semibold text-gray-500 hover:text-gray-800 dark:hover:text-gray-400"
+        class="inline-block py-2 rounded-lg font-semibold text-gray-500 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-300"
       >
         <span class="text-sm flex items-center gap-2">
           <IconPhArrowUp size="16" weight="bold" aria-hidden="true" />
