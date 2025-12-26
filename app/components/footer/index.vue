bbc0dfb4a9be69f15dad0b09c1cbc587a92a6a79
Dinobergue Viana <dinbergueviana@MacBook-Air-de-Dinbergue.local>
2025-12-26 19:07:33 -0300

    style: change footer items position

--- app/components/footer/index.vue
+++ app/components/footer/index.vue
@@ -2,15 +2,15 @@
   <footer
     class="px-6 py-2 bg-white dark:bg-gray-800 flex items-center justify-between md:justify-end"
   >
-    <div class="w-[100%] md:w-[50%] flex items-center justify-between">
-      <p class="text-center text-gray-500 font-normal text-md">
+    <div class="w-[100%] flex items-center justify-center md:justify-between flex-col-reverse md:flex-row">
+      <p class="text-center text-gray-500 dark:text-gray-400  font-normal text-sm">
         © 2025 Dinobergue Viana
       </p>
 
       <NuxtLink
         href="#hero"
         aria-label="Voltar para o topo"
-        class="inline-block py-2 rounded-lg font-semibold text-gray-500 hover:text-gray-800 dark:hover:text-gray-400"
+        class="inline-block py-2 rounded-lg font-normal text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-500"
       >
         <span class="text-sm flex items-center gap-2">
           <IconPhArrowUp size="16" weight="bold" aria-hidden="true" />
