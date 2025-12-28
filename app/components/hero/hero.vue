e280507fb039fcbf26c38e560f68eeaf6d33efcd
Dinobergue Viana <dinbergueviana@MacBook-Air-de-Dinbergue.local>
2025-12-28 11:52:35 -0300

    feat: add pulse animation

--- app/components/hero/hero.vue
+++ app/components/hero/hero.vue
@@ -3,13 +3,13 @@
     class="bg-white dark:bg-gray-900 relative flex flex-col items-center justify-center h-screen px-4 transition-colors duration-300 ease-in-out"
     id="hero"
   >
-    <FadeInSection>
+    <FadeInSection class="text-center mb-3">
       <h1 class="font-bold text-5xl dark:text-gray-200">
         {{ $t("welcome") }} Dinobergue Viana
       </h1>
     </FadeInSection>
 
-    <FadeInSection>
+    <FadeInSection class="">
       <div class="mb-2 text-gray-600 dark:text-gray-300">
         <span> {{ $t("role") }} </span>
       </div>
@@ -60,7 +60,24 @@
           </template>
         </Tooltip>
       </div>
-      <DownloadResumeButton />
+
+      <div class="relative mt-4 flex items-center gap-1">
+      <DownloadResumeButton class="mr-4" />
+
+      <!-- Container dos círculos -->
+      <div class="relative flex items-center justify-center w-4 h-4">
+        <!-- Círculo animado (pulse) -->
+        <div class="absolute inset-0 rounded-full bg-green-600/50 dark:bg-green-400/50 pulse"></div>
+
+        <!-- Círculo fixo (interno) -->
+        <div class="w-1.5 h-1.5 rounded-full bg-green-600 dark:bg-green-400 z-10"></div>
+      </div>
+
+      <span class="text-green-600 dark:text-green-400 text-sm font-semibold">
+        Disponível para novos projetos
+      </span>
+    </div>
+
     </FadeInSection>
 
   </section>
@@ -70,3 +87,36 @@
 import Tooltip from "../global/tooltip/tooltip.vue";
 import DownloadResumeButton from "../buttons/DownloadResumeButton.vue";
 </script>
+
+<style scoped>
+  @keyframes pulse-animation {
+    0% {
+      scale: 0;
+      opacity: 0;
+    }
+
+    25% {
+      scale: 0.2;
+      opacity: 0.5;
+    }
+
+    50% {
+      scale: 0.4;
+      opacity: 1;
+    }
+
+    75% {
+      scale: 0.8;
+      opacity: 0.5;
+    }
+
+    100% {
+      scale: 1;
+      opacity: 0;
+    }
+  }
+
+  .pulse {
+    animation: pulse-animation ease-in-out 1s infinite;
+  }
+</style>
