b9bcc81395db71c57596fcc3032d7e45db1f9862
Dinobergue Viana <dinbergueviana@MacBook-Air-de-Dinbergue.local>
2026-01-14 01:16:40 -0300

    feat: add pixel image on about me page

--- app/pages/me/index.vue
+++ app/pages/me/index.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1>Sobre mim</h1>
+  <section class="mt-10 w-full">
+    <div class="flex items-center justify-center">
+      <img class="w-[100px] h-[100px] rounded-full shadow-md border" src="/images/pixel-me.png" alt="Minha foto">
+    </div>
+  </section>
 </template>
